66d6a80b757a7b5f1a18a4567495640fe5ace438
lkj35 <lkj35@LKJ-SURFACEBOOK>
2017-09-29 10:38:30 +0800

    Ready to Add Telerik function

---
+++ WebServicePorject/App_Code/queryservice.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for queryservice
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class queryservice : System.Web.Services.WebService
+{
+
+    public queryservice()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public string HelloWorld()
+    {
+        return "Hello World";
+    }
+
+}
