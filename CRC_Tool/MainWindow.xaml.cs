103d63a65ca6a0b1ede5ff955f2571013ce9d8ab
lkj35 <lkj35@LKJ-SURFACEBOOK>
2019-07-01 21:27:25 +0800

    在创建GM的工具之前备份。

--- CRC_Tool/MainWindow.xaml.cs
+++ CRC_Tool/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using YyWsnDeviceLibrary;
 
 namespace CRC_Tool
 {
@@ -25,131 +26,6 @@ namespace CRC_Tool
         }
 
         /// <summary>
-        /// 判断字符串中是否有非16进制数
-        /// </summary>
-        /// <param name="hexStr">此处输入的字符串已经去除了内部的空格</param>
-        /// <returns></returns>
-        private static UInt16 HexString_isRight(string hexStr)
-        {
-            byte thisValue = 0;
-            UInt16 StrLen = 0;
-
-            // 字符串长度保护
-            if (hexStr.Length > 0xFFFE)
-            {
-                StrLen = 0xFFFE;
-            }
-            else
-            {
-                StrLen = (UInt16)hexStr.Length;
-            }
-
-            // 判断是否含有非16进制字符
-            for (UInt16 iCnt = 0; iCnt < StrLen; iCnt++)
-            {
-                thisValue = (byte)hexStr[iCnt];
-
-                if ((thisValue >= '0' && thisValue <= '9') || (thisValue >= 'a' && thisValue <= 'f') || (thisValue >= 'A' && thisValue <= 'F'))
-                {   // 是16进制数
-                    continue;
-                }
-                else
-                {   // 字符串里含有非16进制数
-                    return iCnt;
-                }
-            }
-
-            return StrLen;
-        }
-
-        /// <summary>
-        /// 将16进制的字符串转换为字节数组
-        /// </summary>
-        /// <param name="s"></param>
-        /// <returns></returns>
-        private static byte[] HexStringToByteArray(string hexStr)
-        {
-            try
-            {
-                // 去除字符串中的空格
-                hexStr = hexStr.Replace(" ", "");
-
-                // 判断字符串中是否有非16进制数
-                UInt16 StrLen = HexString_isRight(hexStr);
-                if (StrLen == 0)
-                {
-                    return null;
-                }
-
-                // 计算字节数组的长度
-                UInt16 BufLen = 0;
-                if (0 == (StrLen % 2))
-                {
-                    BufLen = (UInt16)(StrLen / 2);
-                }
-                else
-                {
-                    BufLen = (UInt16)(StrLen / 2 + 1);
-                }
-
-                // 创建字节数组
-                byte[] Buf = new byte[BufLen];
-
-                // 填充字节数组的内容
-                for (UInt16 iCnt = 0; iCnt < BufLen - 1; iCnt++)
-                {
-                    Buf[iCnt] = (byte)Convert.ToByte(hexStr.Substring(iCnt * 2, 2), 16);
-                }
-                if (0 == (StrLen % 2))
-                {
-                    Buf[BufLen - 1] = (byte)Convert.ToByte(hexStr.Substring((BufLen - 1) * 2, 2), 16);
-                }
-                else
-                {
-                    Buf[BufLen - 1] = (byte)Convert.ToByte(hexStr.Substring((BufLen - 1) * 2, 1), 16);
-                }
-
-                return Buf;
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        /// <summary>
-        /// 计算CRC
-        /// </summary>
-        /// <param name="polynomial"></param>
-        /// <param name="seed"></param>
-        /// <param name="input"></param>
-        /// <param name="nbrOfBytes"></param>
-        /// <returns></returns>
-        UInt16 CRC16(UInt16 polynomial, UInt16 seed, byte[] input, UInt16 nbrOfBytes)
-        {
-            UInt16 crc = seed, bit = 0, byteCtr = 0;
-
-            for (byteCtr = 0; byteCtr < nbrOfBytes; byteCtr++)
-            {
-                crc ^= (UInt16)(input[byteCtr] << 8);
-
-                for (bit = 8; bit > 0; bit--)
-                {
-                    if ((crc & 0x8000) == 0)
-                    {
-                        crc <<= 1;
-                    }
-                    else
-                    {
-                        crc = (UInt16)((crc << 1) ^ polynomial);
-                    }
-                }
-            }
-
-            return crc;
-        }
-
-        /// <summary>
         /// 计算CRC
         /// </summary>
         /// <param name="sender"></param>
@@ -161,14 +37,14 @@ namespace CRC_Tool
             tbx_Crc.Text = "";
 
             // 将16进制的字符串转换为字节数组
-            byte[] CrcBuf = HexStringToByteArray(tbx_Buf.Text);
+            byte[] CrcBuf = MyCustomFxn.HexStringToByteArray(tbx_Buf.Text);
             if (CrcBuf == null || CrcBuf.Length == 0)
             {
                 return;
             }
 
             // 获取Seed
-            byte[] SeedBuf = HexStringToByteArray(tbx_Seed.Text);
+            byte[] SeedBuf = MyCustomFxn.HexStringToByteArray(tbx_Seed.Text);
             UInt16 Seed = 0;
             if (SeedBuf == null)
             {
@@ -184,7 +60,7 @@ namespace CRC_Tool
             }
 
             // 获取生成多项式
-            byte[] PolynomialBuf = HexStringToByteArray(tbx_Polynomial.Text);
+            byte[] PolynomialBuf = MyCustomFxn.HexStringToByteArray(tbx_Polynomial.Text);
             UInt16 Polynomial = 0;
             if (PolynomialBuf == null)
             {
@@ -200,8 +76,8 @@ namespace CRC_Tool
             }
 
             // 计算CRC
-            UInt16 CRC = CRC16(Polynomial, Seed, CrcBuf, (UInt16)CrcBuf.Length);
-
+            //UInt16 CRC = MyCustomFxn.CRC16(Polynomial, Seed, CrcBuf, 0, (UInt16)CrcBuf.Length);
+            UInt16 CRC = MyCustomFxn.CRC16_By_CHISCDC(CrcBuf, 0, (UInt16)CrcBuf.Length);
 
             // 显示字节数组的长度
             tbx_Len.Text = CrcBuf.Length.ToString();
---
+++ Split_Tool/MainWindow.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Timers;
+using System.IO;
+
+namespace Split_Tool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private static readonly object obj = new object();      // 互斥锁
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 单个文件分割函数， 可以将指定文件分割为若干个子文件
+        /// fileIn: 子文件名形如"D:\\file.rar@_1.split"
+        /// MaxLenKB: 单个子文件最大容量，单位：KB
+        /// delete: 分割完成后是否删除原文件
+        /// </summary>
+        public Int16 fileSplit(String fileIn, UInt32 MaxLenKB, bool delete)
+        {
+            //输入文件校验
+            if (fileIn == null || System.IO.File.Exists(fileIn) == false)
+            {
+                MessageBox.Show("文件:\n\n" + fileIn + "\n\n不存在！");
+                return -1;
+            }
+
+            //取文件名和后缀
+
+            //从文件创建输入流
+            FileStream FileIn = new FileStream(fileIn, FileMode.Open);
+
+            // 若本身就符合大小要求，则不必再拆分
+            if (FileIn.Length <= MaxLenKB * 1024)
+            {
+                FileIn.Close();                                     // 关闭输入流
+                MessageBox.Show("文件:\n\n" + fileIn + "\n\n符合大小要求，无需再拆分！");
+                return 1;
+            }
+
+            byte[] ReadBuf = new byte[1024];    // 流读取,缓存空间
+            UInt32 FileSize = 0;                // 记录子文件累积读取的大小，单位：KB
+            UInt32 FileIndex = 0;               // 子文件序号
+
+            string fileName = System.IO.Path.GetFileName(fileIn);   // 文件名
+            string extension = System.IO.Path.GetExtension(fileIn); // 扩展名 
+            string fileNameWithoutExtension = System.IO.Path.GetFileNameWithoutExtension(fileIn);// 没有扩展名的文件名
+            string directory = System.IO.Path.GetDirectoryName(fileIn); // 路径
+
+            FileStream FileOut = null;          //输出流
+            int ReadLen = 0;                    //每次实际读取的字节大小
+            while (ReadLen > 0 || (ReadLen = FileIn.Read(ReadBuf, 0, ReadBuf.Length)) > 0) //读取数据
+            {
+                //创建分割后的子文件，已有则覆盖
+                if (FileSize == 0)
+                {
+                    FileOut = new FileStream(directory + "\\" + fileNameWithoutExtension + "_" + ++FileIndex + extension, FileMode.Create);
+                }
+
+                //输出，缓存数据写入子文件
+                FileOut.Write(ReadBuf, 0, ReadLen);
+                FileOut.Flush();
+
+                //预读下一轮缓存数据
+                ReadLen = FileIn.Read(ReadBuf, 0, ReadBuf.Length);
+                if (++FileSize >= MaxLenKB || ReadLen == 0)     // 子文件达到指定大小，或文件已读完
+                {
+                    FileOut.Close();                            // 关闭当前输出流
+                    FileSize = 0;
+                }
+            }
+
+            FileIn.Close();                                     // 关闭输入流
+
+            if (delete == true)
+            {
+                System.IO.File.Delete(fileIn);                  // 删除源文件
+            }
+
+            MessageBox.Show("文件:\n\n" + fileIn + "\n\n拆分完成！");
+
+            return 0;
+        }
+
+        private void btn_Split_Click(object sender, RoutedEventArgs e)
+        {
+            //System.IO.FileInfo fileInfo = null;
+            //StreamWriter streamWriter = null;
+
+            try
+            {
+                fileSplit(tbx_File.Text, 20 * 1024, false);
+            }
+            catch (Exception ex)
+            {
+                tbx_Status.Text = ex.Message;
+            }
+        }
+    }
+}
