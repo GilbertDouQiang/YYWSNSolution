3e04597540e403a775edefb4ca187145f7bbeaf1
lkj35 <lkj35@LKJ-SURFACEBOOK>
2021-01-05 20:55:30 +0800

    在HyperWSN_Setup_M30中添加CC1310的串口升级功能之前，备份。

---
+++ MD5_Tool/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using YyWsnDeviceLibrary;
+
+namespace MD5_Tool
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.Title += "  v" +
+               System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString();
+        }
+
+        /// <summary>
+        /// 计算MD5
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btn_Calc_Click(object sender, RoutedEventArgs e)
+        {
+            byte[] encrypt = Encoding.ASCII.GetBytes(tbx_Buf.Text);             // 明文
+            if (encrypt == null || encrypt.Length > 64)
+            {
+                MessageBox.Show("明文字符串编码错误或长度超限！");
+                return;
+            }
+
+            MD5 aMD5 = new MD5(tbx_Key.Text);
+
+            byte[] decrypt = aMD5.Calc(encrypt, (UInt32)encrypt.Length);        // 密文
+            if (decrypt == null || decrypt.Length != 16)
+            {
+                MessageBox.Show("MD5计算失败！");
+                return;
+            }
+
+            // 显示密文
+            tbx_Result.Text = "";
+
+            UInt16 iX;
+            for (iX = 0; iX < (UInt16)decrypt.Length; iX++)
+            {
+                if (iX == (UInt16)(decrypt.Length - 1))
+                {
+                    tbx_Result.Text += decrypt[iX].ToString("X2");
+                }
+                else
+                {
+                    tbx_Result.Text += decrypt[iX].ToString("X2") + " ";
+                }
+            }
+        }
+    }
+}
---
+++ ReceiveFile/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReceiveFile
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            FindComport();
+        }
+
+        private void FindComport()
+        {
+            cbSerialPort.Items.Clear();
+            string[] spis = SerialPortHelper.GetSerialPorts();
+            foreach (var portname in spis)
+            {
+                cbSerialPort.Items.Add(portname);
+            }
+            if (cbSerialPort.Items.Count > 0)
+            {
+                cbSerialPort.SelectedIndex = 0;
+            }
+        }
+
+        private void btnFindComport_Click(object sender, RoutedEventArgs e)
+        {
+            FindComport();
+        }
+
+        private void btnOpenComport_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnOpenComport.Content.ToString() == "Open")
+            {
+                SerialPort = new SerialPortHelper();
+                SerialPort.SerialPortReceived += Comport_SerialPortReceived;
+                string portname = SerialPortHelper.GetSerialPortName(cbSerialPort.SelectedValue.ToString());
+                SerialPort.InitCOM(portname, 115200);
+                if (SerialPort.OpenPort())
+                {
+                    btnOpenComport.Content = "Close";
+                }
+            }
+            else
+            {
+                if (SerialPort != null)
+                {
+                    SerialPort.ClosePort();
+                    btnOpenComport.Content = "Open";
+                }
+            }
+        }
+    }
+}
